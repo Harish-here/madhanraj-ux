82d75e934e95be8893d17a1286c66f1453a58f48
harish <harish@madstreetden.com>
2021-06-24 20:26:30 +0530

    Added visual design

--- pages/visual-designs.jsx
+++ pages/visual-designs.jsx
@@ -1,7 +1,13 @@
 export default function VisualDesigns() {
   return (
     <div className="flex items-center justify-center w-full h-screen font-bold text-lg">
-      &lt;Visual Designs&gt;
+      <iframe
+        id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="960"
+        height="600"
+        src="https://burrata.invisionapp.com/console/share/UA2FAWSLTJ/587963387"
+      ></iframe>
     </div>
   );
 }
